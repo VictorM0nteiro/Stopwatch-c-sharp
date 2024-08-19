1227221411f032402bf0383d8830b1e1572d7c01
Victor Monteiro <devvictor.io@gmail.com>
2024-08-19 15:23:14 -0300

    second commit
    added a stating function and the menu

--- Program.cs
+++ Program.cs
@@ -7,11 +7,23 @@ namespace Stopwatch
     {
         static void Main(string[] args)
         {
-            Start();
+            Menu();
         }
 
-        static void Start(){
-            int time = 10;
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundos => 10s = 10 segundos");
+            Console.WriteLine("M = Minutos => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower(); // Tolower vai converter tudo lido para minúsculo
+            Console.WriteLine(data);
+        }
+
+        static void Start(int time)
+        {
             int currentTime = 0;
 
             while( currentTime != time)
@@ -20,8 +32,11 @@ namespace Stopwatch
                 currentTime++;  
                 Console.WriteLine(currentTime); 
                 Thread.Sleep(1000); // Thread.Sleep(1000); em C# faz com que a thread atual (onde o código está sendo executado) pause ou "durma" por um tempo especificado, antes de continuar a execução do próximo código. Esse tempo é medido em milissegundos.
-                
             }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado");
+            Thread.Sleep(2500);
         }
     }
 }
